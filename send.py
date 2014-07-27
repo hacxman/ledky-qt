b78b324cb48b1bd7e159ec420277f19075df88cb
Maros Zatko <hacxman@gmail.com>
2014-07-28 00:15:28 +0200

    send: sends argument

--- send.py
+++ send.py
@@ -5,8 +5,12 @@ class ServerIfc(QtDBus.QDBusAbstractInterface):
   def __init__(self, service, path, connection, parent=None):
       super(ServerIfc, self).__init__(service, path, 'cz.base48.Basebar.C', connection, parent)
 
-  def newProductCode(self):
-    m = self.call('newProductCode', 'EAN-13:5901234123457')
+  def newProductCode(self, code=None):
+    if code is None:
+      code = 'EAN-13:5901234123457'
+      code = 'EAN-13:4029764001883'
+
+    m = self.call('newProductCode', code)
     print m.errorMessage()
 
 if __name__ == '__main__':
@@ -17,6 +21,9 @@ if __name__ == '__main__':
   server = ServerIfc('cz.base48.Basebar', '/C',
             QtDBus.QDBusConnection.sessionBus())
 
-  server.newProductCode()
+  if len(sys.argv) == 1:
+    server.newProductCode()
+  else:
+    server.newProductCode(sys.argv[1])
 
 #  sys.exit(app.exec_())
